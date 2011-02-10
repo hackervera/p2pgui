dcb74b894d0e4f5b12bbe891e6bed6e0a3ed7b87
tjgillies <tjgillies@gmail.com>
2011-02-10 01:49:11 -0800

    its good to commit

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 #ShoutOut to @donpdonp for suggesting Shoes for gui
 require 'rubygems'
-
+#Thread.abort_on_exception = true
 #require 'ezcrypto/signer'
 SHOES_APP = File.expand_path(__FILE__)
 require 'shoes'
@@ -11,6 +11,30 @@ require 'ezcrypto'
 require 'ezsig'
 require 'digest/sha1'
 require 'json'
+require 'socket'
+
+#third time i ported this fucking function, thanks @donpdonp and @reidab
+def RSASign(sHashHex, pub, priv) #this function copied from the rsa.js script included in Tom Wu's jsbn library
+  #n = new Math_BigInteger(pub,16);
+  sMid = "";	fLen = ((pub.size*8) / 4) - sHashHex.length - 6
+  #p pub.to_bits
+  i = 0
+  p "FLEN #{fLen}"
+  sMid = "f"*fLen
+  p "SMID: #{sMid.length}"
+  hPM = "0001" + sMid + "00" + sHashHex #this pads the hash to desired length - not entirely sure whether those 'ff' should be random bytes for security or not
+  x = hPM.to_i(16) #turn the padded message into a jsbn BigInteger object
+  #$d = new Math_BigInteger($priv,16)
+  p "X: #{x}, PRIV: #{priv}, PUB: #{pub}"
+  return Math.power_modulo(x,priv,pub) #$x->modPow($d, $n)
+end
+
+def RSAVerify(modulus, message, signature)
+  n = modulus.to_i(16)
+  x = signature.gsub(/[ \n]+/, "").to_i(16)
+  return Math.power_modulo(x,"10001".to_i(16), n).to_s(16).gsub(/^1f+00/, '') == Digest::SHA1.hexdigest(message)
+end
+
 
 class Bignum
   def to_bits(num = self)
@@ -29,68 +53,111 @@ def Math.power_modulo(b, p, m)
   end
 end
 
-
-#third time i ported this fucking function, thanks @donpdonp and @reidab
-def RSASign(sHashHex, pub, priv) #this function copied from the rsa.js script included in Tom Wu's jsbn library
-  #n = new Math_BigInteger(pub,16);
-  sMid = "";	fLen = ((pub.size*8) / 4) - sHashHex.length - 6
-  #p pub.to_bits
-  i = 0
-  p "FLEN #{fLen}"
-=begin
-  fLen.times do |blah|
-    sMid += "ff"
-    i += 2
+class StackObject << Array
+  def initialize
+    @stacks = []
   end
-=end
-  sMid = "f"*82
-  p "SMID: #{sMid.length}"
-  hPM = "0001" + sMid + "00" + sHashHex #this pads the hash to desired length - not entirely sure whether those 'ff' should be random bytes for security or not
-  x = hPM.to_i(16) #turn the padded message into a jsbn BigInteger object
-  #$d = new Math_BigInteger($priv,16)
-  p "X: #{x}, PRIV: #{priv}, PUB: #{pub}"
-  return Math.power_modulo(x,priv,pub) #$x->modPow($d, $n)
-end
+  def add
+stacks = []
 
 
-#key = EzCrypto::Key.generate
-#p key
-signer = EzCrypto::Signer.generate
-#p signer.methods - Object.methods
-msg = 'something secret'
-sha1 = Digest::SHA1.hexdigest(msg)
-#p sha1 #.methods - Object.methods
-#n = signer.public_key.n.to_int
-#d = signer.private_key.d.to_int
-n= 0xcc0f26cd602216e149fe8c2b4027293cd05cd5ccb8720d48a3e50c11c4ce5402cbd3d186e05f5bf15acb078c945f3ca99d0f1b4c7a01722704981afe7ba58f5b
-d = 0x880b6df62caa6d90a3d166480b8c504cf029848ce947789dbe4f1d7dd7352c0243dc83e5c7704632b0ad55e9086c11deb7bbda791b59a2eca8da99be6dde6a79
-sig = RSASign(sha1, n, d).to_s(16)
-telex = {"+key" => n.to_s(16), "_hop" => 1,"+end" => "8bf1cce916417d16b7554135b6b075fb16dd26ce","_to"=>"208.68.163.247:42424", "+sig"=>sig, "+message"=>msg }.to_json
 
-#p signer.public_key.e.to_int
-#sig = signer.sign("Happy valentines day mother fuckers")
-#p sig.methods - Object.methods
-#p sig.class
-#p sig.to_i(base=16)
 def stacker
+
   colors = ['#00f','#f00', '#0f0']
   colors.each do |color|
     baz = stack :width => 30, :height => 30 do |this|
+      stacks << baz
       fill color
       stroke color
       foo = self
       click do |b,x,y|
         #alert("clicked on #{color} square")
         p baz
-        baz.fill purple
-        baz.stroke purple
+        thisColor = "#%06x" % (rand * 0xffffff)
+        baz.fill thisColor
+        baz.stroke thisColor
         baz.rect(0,0,30,30)
+        msg = thisColor
+        p msg
+        sha1 = Digest::SHA1.hexdigest(msg)
+        p sha1
+        n= 0xcc0f26cd602216e149fe8c2b4027293cd05cd5ccb8720d48a3e50c11c4ce5402cbd3d186e05f5bf15acb078c945f3ca99d0f1b4c7a01722704981afe7ba58f5b
+        d = 0x880b6df62caa6d90a3d166480b8c504cf029848ce947789dbe4f1d7dd7352c0243dc83e5c7704632b0ad55e9086c11deb7bbda791b59a2eca8da99be6dde6a79
+        sig = RSASign(sha1, n, d).to_s(16)
+        p "SIG: #{sig}"
+        telex = {"+key" => n.to_s(16), "_hop" => 1,"+end" => "8bf1cce916417d16b7554135b6b075fb16dd26ce","_to"=>"208.68.163.247:42424", "+sig"=>sig, "+message"=>msg }.to_json
+        p "TELEX: #{telex}"
+        sock = UDPSocket.new
+        
+        data = telex
+        sock.send(data, 0, 'telehash.org', 42424)
+        sock.close
       end
       rect(0,0,30,30)
     end
     p "foo"
   end
 end
-Shoes.app :width => 60, :height => 32 do
-  stacker
+class UDPMessage
+  def initialize(socket)
+    @socket = socket
+  end
+  attr_accessor :hostname, :port, :body
+  def send_message
+    @socket.send self.body, 0, self.hostname, self.port
+  end
 end
+
+def start_udpserver
+  p "Starting server"
+  socket = UDPSocket.new
+  p "binding server"
+  p socket.bind("0.0.0.0",8888)
+  #sockaddr = Socket.pack_sockaddr_in( 8888, 'localhost' )
+  #p socket.bind(sockaddr)
+  message = UDPMessage.new(socket)
+  message.hostname = "telehash.org"
+  message.port = 42424
+  message.body = {"+end"=>"38666817e1b38470644e004b9356c1622368fa57"}.to_json
+  p "sending message"
+  p message.send_message
+  loop do
+    p "waiting for message"
+    response, addr = socket.recvfrom(50000)
+    response_json = JSON.parse(response)
+    #p response
+    p response_json
+    line = nil
+    if response_json.has_key? "_ring"
+      line = response_json["_ring"]
+      message.body = {".tap"=>[{"has" => ["+key"]}],"_line"=>line, "_to"=>"208.68.163.247:42424"}.to_json
+      p "Sending tap"
+      message.send_message
+    end
+    if response_json.has_key? "+key" 
+      if RSAVerify(response_json["+key"], response_json["+message"], response_json["+sig"])
+        stacks.each do |stack|
+          stack.fill black
+          stack.stroke black
+          stack.rect(0,0,30,30)
+        end
+      end
+    end
+    
+  end
+  rescue => e
+    p e
+    p e.backtrace
+end
+  
+Shoes.app :width => 60, :height => 32 do
+
+  Thread.new do
+    start_udpserver
+  end
+
+  stacker
+
+end
+
