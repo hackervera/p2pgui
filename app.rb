86dca73e49e2e518a78080ee31ba572e9d96918b
Don Park <don@donpark.org>
2011-02-24 18:33:32 -0800

    moved RSA into its own class. put local methods inside the Shoes block

--- app.rb
+++ app.rb
@@ -6,25 +6,38 @@ require 'digest/sha1'
 require 'json'
 require 'socket'
 
-#third time i ported this fucking function, thanks @donpdonp and @reidab
-def RSASign(sHashHex, pub, priv) #this function copied from the rsa.js script included in Tom Wu's jsbn library
-  sMid = "";	fLen = ((pub.size*8) / 4) - sHashHex.length - 6
-  i = 0
-  #p "FLEN #{fLen}"
-  sMid = "f"*fLen
-  #p "SMID: #{sMid.length}"
-  hPM = "0001" + sMid + "00" + sHashHex #this pads the hash to desired length - not entirely sure whether those 'ff' should be random bytes for security or not
-  x = hPM.to_i(16) #turn the padded message into a jsbn BigInteger object
-  #p "X: #{x}, PRIV: #{priv}, PUB: #{pub}"
-  return Math.power_modulo(x,priv,pub) #$x->modPow($d, $n)
+class GilliesRSA
+  #third time i ported this fucking function, thanks @donpdonp and @reidab
+  def self.sign(sHashHex, pub, priv) #this function copied from the rsa.js script included in Tom Wu's jsbn library
+    sMid = ""
+    fLen = ((pub.size*8) / 4) - sHashHex.length - 6
+    i = 0
+    #p "FLEN #{fLen}"
+    sMid = "f"*fLen
+    #p "SMID: #{sMid.length}"
+    hPM = "0001" + sMid + "00" + sHashHex #this pads the hash to desired length - not entirely sure whether those 'ff' should be random bytes for security or not
+    x = hPM.to_i(16) #turn the padded message into a jsbn BigInteger object
+    #p "X: #{x}, PRIV: #{priv}, PUB: #{pub}"
+    return Math.power_modulo(x,priv,pub) #$x->modPow($d, $n)
+  end
+
+  def self.verify(modulus, message, signature)
+    n = modulus.to_i(16)
+    x = signature.gsub(/[ \n]+/, "").to_i(16)
+    return Math.power_modulo(x,"10001".to_i(16), n).to_s(16).gsub(/^1f+00/, '') == Digest::SHA1.hexdigest(message)
+  end
 end
 
-def RSAVerify(modulus, message, signature)
-  n = modulus.to_i(16)
-  x = signature.gsub(/[ \n]+/, "").to_i(16)
-  return Math.power_modulo(x,"10001".to_i(16), n).to_s(16).gsub(/^1f+00/, '') == Digest::SHA1.hexdigest(message)
+class UDPMessage
+  def initialize(socket)
+    @socket = socket
+    @br = 0
+  end
+  attr_accessor :hostname, :port, :body, :line, :me, :br
+  def send_message
+    @socket.send self.body, 0, self.hostname, self.port
+  end
 end
-
 
 def Math.power_modulo(b, p, m)
   if p == 1
@@ -38,80 +51,71 @@ def Math.power_modulo(b, p, m)
 end
 
 
-def stacker
-  colors = []
-  100.times do 
-    colors << white
-  end
-  #colors = ['#00f','#f00', '#0f0']
-  matrix_y = 1
-  matrix_x = 1
-  colors.each do |color|
-    baz = stack :width => 30, :height => 30 do |this|
-      
-      fill color
-      stroke color
-      foo = self
-      click do |b,x,y|
-        #alert("clicked on #{color} square")
-        p baz
-        thisColor = "#%06x" % (rand * 0xffffff)
-        matrix_x = $stacks.select do |stack|
-          stack[0] == baz
-        end.first[1]
-        baz.fill thisColor
-        baz.stroke thisColor
-        baz.rect(0,0,30,30)
-        msg = thisColor
-        p msg
-        sha1 = Digest::SHA1.hexdigest(msg)
-        p sha1
-        #modulus hex encoded
-        n= 0xcc0f26cd602216e149fe8c2b4027293cd05cd5ccb8720d48a3e50c11c4ce5402cbd3d186e05f5bf15acb078c945f3ca99d0f1b4c7a01722704981afe7ba58f5b
-        #decryption exponent hex encoded
-        d = 0x880b6df62caa6d90a3d166480b8c504cf029848ce947789dbe4f1d7dd7352c0243dc83e5c7704632b0ad55e9086c11deb7bbda791b59a2eca8da99be6dde6a79
-        sig = RSASign(sha1, n, d).to_s(16)
-        p "SIG: #{sig}"
-        telex = { "+key" => n.to_s(16), 
-                  "_hop" => 1,
-                  "+end" => "8bf1cce916417d16b7554135b6b075fb16dd26ce",
-                  "_to"=>"208.68.163.247:42424", 
-                  "+sig"=>sig, 
-                  "+message"=>msg,
-                  "+matrix_x" => matrix_x,
-                  "+matrix_y" => matrix_y
-                }.to_json
-        p "TELEX: #{telex}"
-        sock = UDPSocket.new
-        
-        data = telex
-        sock.send(data, 0, 'telehash.org', 42424)
-        sock.close
-      end
-      rect(0,0,30,30)
+Shoes.app :width => 500, :height => 300 do
+
+  def stacker
+    colors = []
+    100.times do 
+      colors << white
     end
-    #p baz.methods - Object.methods
+    #colors = ['#00f','#f00', '#0f0']
+    matrix_y = 1
+    matrix_x = 1
+    colors.each do |color|
+      baz = stack :width => 30, :height => 30 do
+      
+        fill color
+        stroke color
+        foo = self
+        click do |b,x,y|
+          #alert("clicked on #{color} square")
+          p baz
+          thisColor = "#%06x" % (rand * 0xffffff)
+          matrix_x = $stacks.select do |stack|
+            stack[0] == baz
+          end.first[1]
+          baz.fill thisColor
+          baz.stroke thisColor
+          baz.rect(0,0,30,30)
+          msg = thisColor
+          p msg
+          sha1 = Digest::SHA1.hexdigest(msg)
+          p sha1
+          #modulus hex encoded
+          n= 0xcc0f26cd602216e149fe8c2b4027293cd05cd5ccb8720d48a3e50c11c4ce5402cbd3d186e05f5bf15acb078c945f3ca99d0f1b4c7a01722704981afe7ba58f5b
+          #decryption exponent hex encoded
+          d = 0x880b6df62caa6d90a3d166480b8c504cf029848ce947789dbe4f1d7dd7352c0243dc83e5c7704632b0ad55e9086c11deb7bbda791b59a2eca8da99be6dde6a79
+          sig = GilliesRSA.sign(sha1, n, d).to_s(16)
+          p "SIG: #{sig}"
+          telex = { "+key" => n.to_s(16), 
+                    "_hop" => 1,
+                    "+end" => "8bf1cce916417d16b7554135b6b075fb16dd26ce",
+                    "_to"=>"208.68.163.247:42424", 
+                    "+sig"=>sig, 
+                    "+message"=>msg,
+                    "+matrix_x" => matrix_x,
+                    "+matrix_y" => matrix_y
+                  }.to_json
+          p "TELEX: #{telex}"
+          sock = UDPSocket.new
+        
+          data = telex
+          sock.send(data, 0, 'telehash.org', 42424)
+          sock.close
+        end
+        rect(0,0,30,30)
+      end
+      #p baz.methods - Object.methods
 
-    $stacks << [ baz, matrix_x, matrix_y ]
+      $stacks << [ baz, matrix_x, matrix_y ]
     
-    p "foo"
-    matrix_x += 1
+      p "foo"
+      matrix_x += 1
+    end
+    rescue => e
+      p e 
+      puts e.backtrace
   end
-  rescue => e
-    p e 
-    puts e.backtrace
-end
-
-class UDPMessage
-  def initialize(socket)
-    @socket = socket
-    @br = 0
-  end
-  attr_accessor :hostname, :port, :body, :line, :me, :br
-  def send_message
-    @socket.send self.body, 0, self.hostname, self.port
-  end
-end
 
 def ping_loop(message)
   loop do
@@ -156,7 +160,7 @@ def start_udpserver
       counter += 1
     end
     if response_json.has_key? "+key" 
-      if RSAVerify(response_json["+key"], response_json["+message"], response_json["+sig"])
+      if GilliesRSA.verify(response_json["+key"], response_json["+message"], response_json["+sig"])
         #p $stacks
         $stacks.each do |stack|
           #p stack[1]
@@ -177,7 +181,6 @@ def start_udpserver
     puts e.backtrace
 end
   
-Shoes.app :width => 500, :height => 300 do
 
   $stacks = []
   Thread.new do
